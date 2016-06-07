15cbff55d88e728cca49c97b3b26831b71a17bd8
Clare Snyder <clare.d.snyder@dartmouth.edu>
2016-06-07 15:08:13 -0400

    Update ANA_Rat_Data.m
    more efficient version than previous

--- ANA_Rat_Data.m
+++ ANA_Rat_Data.m
@@ -1,106 +1,101 @@
+%Adjusted version of LOOP_CORREGISTRATION.m
+%Uses one fluorescent image (800 nm) and one H&E (or similar) image
 
-    [filename, textpath] = uigetfile([cd filesep '*.txt']);
+%BEFORE RUNNING CODE: Make sure to edit the code based on the orientation
+%of the two images
 
-    % load the filepaths for all the available files.
-    % textpath = 'X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\Corregistered Data Files\File Path Info\';
-    % filename = '404A1.txt';
+%Upload text file specifying location of the two images
+[filename, textpath] = uigetfile([cd filesep '*.txt']);
 
-    fileID = fopen([textpath filename],'r');
+% load the filepaths for all the available files.
+% textpath = 'X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\Corregistered Data Files\File Path Info\';
+% filename = '404A1.txt';
 
-    % determine how many different paths are saved in the text file.
-    i=1;
-    tline = fgetl(fileID);
-    while ischar(tline)
-        pth{i} = tline;
-        tline = fgetl(fileID);
-        i = i + 1;
-    end
-    fclose(fileID);
+fileID = fopen([textpath filename],'r');
 
-    if (length(pth) >= 2)     % has to be at least 2 images to corregister.
-        he_path = 'X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\F98 EGFR_pos B04\hico\'
-        imHE.fname = filename(1:end-4);
-    
-        
-        % CHANGE IF H&E IMAGE IS ROTATED COMPARED WITH FLUORESCENCE IMAGE.
-        %imHE.I = fliplr( flipud(imread([pth{2} '.TIF'])) );
-        %   imHE.I = flipud(imread([pth{4} '.TIF']));
-%            imHE.I = ((imread([pth{4} '.TIF'])) );
-%         imHE.I = rot90(fliplr(imread([pth{4} '.TIF'])));
-        %h2 = imagesc(imHE.I); axis image; axis off; drawnow;
-        
-        %FOR ANA's data, uncomment first, fifth lines (not indented)
-        imHE.I = (imread([pth{2} '.TIF']));
-            % imHE.I = flipud(imread([pth{4} '.TIF']));
-            %         imHE.I = ((imread([pth{4} '.TIF'])) );
-            %         imHE.I = rot90(fliplr(imread([pth{4} '.TIF'])));
-        h2 = imagesc(imHE.I); axis image; axis off; drawnow;
-      
-        im1 = load_images(pth{1});
-        im2 = load_images(pth{1}, 0);
-        close all;
-        T = [0 0]; %<- translation, since it is usually still off a little (no idea why!).
-        [ im1a,im2a ] = imalign( im1, im2, 0, T );
-         im_blend = cat( 3,4.* ( nm(im2a.I_a)) .* 0.5,zeros(size(im1a.I_a)), 5.* nm(im1a.I_a) );
-        imagesc(im_blend); axis image; axis off;
-        
-        % load third image (IRD680)
-        im3 = load_images(pth{1});
-  
-        % fusion of the first three fluorescence images.
-        T = [0 0]; %<- translation, since it is usually still off a little (no idea why!).
-        [ ~, im3a ] = imalign( im1, im3, 0, T );
+% determine how many different paths are saved in the text file.
+i=1;
+tline = fgetl(fileID);
+while ischar(tline)
+pth{i} = tline;
+tline = fgetl(fileID);
+i = i + 1;
+end
+fclose(fileID);
 
-        % create 3-channel fusion.
-        
-        im_blend = cat(3, 2.2.*nm(im3a.I_a), 1.8.* nm(im1a.I_a), 0.7 .* nm(im2a.I_a).^1.5);
-        imagesc(im_blend); axis image; axis off
+if (length(pth) >= 2) % has to be at least 2 images to corregister - H&E and fluorescent
+he_path = 'X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\F98 EGFR_pos B04\hico\'
+imHE.fname = filename(1:end-4);
 
-    end
 
-    %% Load Histopathology Data
-    figure('Name','Draw line spanning midline'); imagesc(im1a.I_a);
-    l1 = imline;
-    p = wait(l1);
-    len1 = sqrt((p(1,1) - p(1,2))^2 + ( p(2,1) - p(2,2) )^2);
-    close;
+%%%If the H&E image is rotated compared to the fluor. image, 
+%%%uncomment the first line in the following chunk of text (and comment 2nd). 
+%%%Otherwise, uncomment the second line and comment the first. 
+%%%When using Ana's data, the images should already be aligned (uncomment 2nd line)
 
-    figure('Name','Draw line spanning midline'); imagesc(imHE.I);
-    l2 = imline;
-    p = wait(l2);
-    len2 = sqrt((p(1,1) - p(1,2))^2 + ( p(2,1) - p(2,2) )^2);
-    close;
-    
-    M = len1 ./ len2;
+imHE.I = fliplr( flipud(imread([pth{2} '.TIF'])) );
+%imHE.I = (imread([pth{2} '.TIF']));
+h2 = imagesc(imHE.I); axis image; axis off; drawnow;
 
-    imHE.I_s = imresize(imHE.I,M);
+im1 = load_images(pth{1});
 
-    figure('Name','Draw a circle around the slice');
-    h2 = imagesc(imHE.I_s); axis image; axis off; drawnow;
+close all;
+ 
+[ im1a,im2a ] = imalign( im1, im1, 0 );
+im_blend = cat( 3,4.* ( nm(im2a.I_a)) .* 0.5,zeros(size(im1a.I_a)), 5.* nm(im1a.I_a) );
+imagesc(im_blend); axis image; axis off;
 
-    % circle out the ROI
-    hFH = imfreehand();
-    pos = hFH.getPosition();
+[ ~, im3a ] = imalign( im1, im1, 0 );
 
-    binaryImage = hFH.createMask();
+% create 3-channel fusion.
 
-    % attempt to resize based on im1a.
-    imHE.I_c = cropfromfh( imHE.I_s, pos );
+im_blend = cat(3, 2.2.*nm(im3a.I_a), 1.8.* nm(im1a.I_a), 0.7 .* nm(im2a.I_a).^1.5);
+imagesc(im_blend); axis image; axis off
+ end
 
-    % attempt to corregister H&E sections
-    [HE_reg, nnr, F ] = select_common_points( im1a, imHE, '', 8 );
+%% Load Histopathology Data
+figure('Name','Draw line spanning midline'); imagesc(im1a.I_a);
+l1 = imline;
+p = wait(l1);
+len1 = sqrt((p(1,1) - p(1,2))^2 + ( p(2,1) - p(2,2) )^2);
+close;
 
-    close all;
-    % save('401B_test_case.mat');
-    %% recplace below with
+figure('Name','Draw line spanning midline'); imagesc(imHE.I);
+l2 = imline;
+p = wait(l2);
+len2 = sqrt((p(1,1) - p(1,2))^2 + ( p(2,1) - p(2,2) )^2);
+close;
 
-    %%%
-    [imHE, smpl_pts] = multi_step_tissue_reg(im1a,im2a,im3a, imHE);
-    %%%
+M = len1 ./ len2;
 
-    stats = sample_from_points( smpl_pts, im1a, im2a, im3a );
+imHE.I_s = imresize(imHE.I,M);
 
-    save([textpath imHE.fname '.mat'])
+figure('Name','Draw a circle around the slice');
+h2 = imagesc(imHE.I_s); axis image; axis off; drawnow;
+
+% circle out the ROI
+hFH = imfreehand();
+pos = hFH.getPosition();
+
+binaryImage = hFH.createMask();
+
+% attempt to resize based on im1a.
+imHE.I_c = cropfromfh( imHE.I_s, pos );
+
+% attempt to corregister H&E sections
+[HE_reg, nnr, F ] = select_common_points( im1a, imHE, '', 8 );
+
+close all;
+% save('401B_test_case.mat');
+%% recplace below with
+
+%%%
+[imHE, smpl_pts] = multi_step_tissue_reg(im1a,im2a,im3a, imHE);
+%%%
+
+stats = sample_from_points( smpl_pts, im1a, im2a, im3a );
+
+save([textpath imHE.fname '.mat'])
 
 %end
 
@@ -154,232 +149,3 @@ ihc3
 
 
 save([pathname 'processed\' he_file 'wrp.mat'],'im1a','im2a','im3a','imHE','mask','im_blend');
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-
-
-
-% 
-% 
-% %% Get the logistic analsyis part
-% [stats, mprob] = MAIN_LOGISTIC_ANALYSIS( im1a, im2a, im3a, imHE, mask, 'panel' );
-% set(gcf,'Position',[488   113   890   649]);
-% saveas(gcf,[pathname 'processed\' he_file '.fig'])
-% saveas(gcf,[pathname 'processed\' he_file '.png'])
-% 
-% % warped image
-% options = fuseset( 'model', 'logistic', 'cmap_type', 'sRGB', 'im2_type', 'sRGB', ...
-%                                 'L', 0.6, 'x0', 0.3, 'k', 20);
-% F = fuse_images( nm(mprob), imHE.I_a(1:size(im1a.I_a,1),1:size(im1a.I_a,2),:), goodmap('comet'), options );
-% 
-% figure; imagesc(F); axis image; axis off; colorbar; colormap(goodmap('comet'))
-% 
-% 
-% fnames = {'405B_2','405C_2','407A_2','407B_1','410B_2', '408B_1','406A_2'};
-% 
-% for i = 1:length(fnames)
-% % pick control points, number them after each click.
-% load(['X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\processed\',...
-%     fnames{i} 'wrp.mat'])
-% plot3( median( im1a.I_a(mask.tumor) ), ...
-%        median( im2a.I_a(mask.tumor) ), ...
-%        median( im3a.I_a(mask.tumor) ), ...
-%       'ro','MarkerSize',10);
-% hold on;
-% plot3( median( im1a.I_a(mask.brain) ), ...
-%        median( im2a.I_a(mask.brain) ), ...
-%        median( im3a.I_a(mask.brain) ), ...
-%       'go','MarkerSize',10);
-% 
-% % plot3( im1a.I_a(mask.tumor), ...
-% %        im2a.I_a(mask.tumor), ...
-% %        im3a.I_a(mask.tumor), ...
-% %       'r.','MarkerSize',1);
-% % plot3( im1a.I_a(mask.brain), ...
-% %        im2a.I_a(mask.brain), ...
-% %        im3a.I_a(mask.brain), ...
-% %       'g.','MarkerSize',1);
-% end
-% 
-% fnames = {'405B_2','405C_2','407B_1','410B_2', '408B_1','406A_2'};
-% 
-% h_s = figure;
-% 
-% for i = 1:length(fnames);
-%      load(['X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\processed\',...
-%      fnames{i} 'wrp.mat']);
-% 
-%      [c, c_nm, bnds] = create_profile( im1a, im2a, im3a, mask )
-%      
-%      subplot(3,2,i);
-%     plot(1:400,c_nm(:,1),'g',1:400,c_nm(:,2),'b',1:400,c_nm(:,3),'r',bnds(1)*[1 1],[0 1],'k--',bnds(2)*[1 1],[0 1],'k--')     
-%     
-% end
-% 
-% for i = 1:length(fnames)
-%     
-% end
-% 
-% % 
-% % h1 = figure;
-% % h2 = figure;
-% % hold on;
-% % for i = 1:length(fnames)
-% %      load(['X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\processed\',...
-% %      fnames{i} 'wrp.mat']);
-% % %      plot( median( im1a.I_a(mask.tumor) ), median( im2a.I_a(mask.tumor) ), 'ro','MarkerSize',10 );
-% % %      hold on
-% % %      plot( median( im1a.I_a(mask.brain) ), median( im2a.I_a(mask.brain) ), 'go','MarkerSize',10 );
-% %      figure(h1)
-% %      hold on
-% %      errorbarxy( median( im1a.I_a(mask.tumor) ), median( im2a.I_a(mask.tumor)), ...
-% %          abs(median( im1a.I_a(mask.tumor) ) - prctile( im1a.I_a(mask.tumor) ,25 )), ...
-% %          abs(median( im2a.I_a(mask.tumor) ) - prctile( im2a.I_a(mask.tumor) ,25 )),{'go--', 'g', 'g'});
-% %      hold on;
-% %      errorbarxy( median( im1a.I_a(mask.brain) ), median( im2a.I_a(mask.brain)), ...
-% %          abs(median( im1a.I_a(mask.brain) ) - prctile( im1a.I_a(mask.brain) ,25 )), ...
-% %          abs(median( im2a.I_a(mask.brain) ) - prctile( im2a.I_a(mask.brain) ,25 )),{'ro--', 'r', 'r'});
-% %      hold on;
-% %      
-% %      figure(h2)
-% %      hold on
-% %      errorbarxy( median( im1a.I_a(mask.tumor) ), median( im3a.I_a(mask.tumor)), ...
-% %          abs(median( im1a.I_a(mask.tumor) ) - prctile( im1a.I_a(mask.tumor) ,25 )), ...
-% %          abs(median( im3a.I_a(mask.tumor) ) - prctile( im3a.I_a(mask.tumor) ,25 )),{'go--', 'g', 'g'});
-% %      hold on;
-% %      errorbarxy( median( im1a.I_a(mask.brain) ), median( im3a.I_a(mask.brain)), ...
-% %          abs(median( im1a.I_a(mask.brain) ) - prctile( im1a.I_a(mask.brain) ,25 )), ...
-% %          abs(median( im3a.I_a(mask.brain) ) - prctile( im3a.I_a(mask.brain) ,25 )),{'ro--', 'r', 'r'});
-% %      hold on;
-% %      
-% % end
-% 
-% h1 = figure;
-% hold on;
-% for i = 1:length(fnames)
-%      load(['X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\processed\',...
-%      fnames{i} 'wrp.mat']);
-%      
-%  figure(h1)
-%  covariance = cov([im1a.I_a(mask.tumor) im2a.I_a(mask.tumor)]);
-%  [V, eigenval ] = eig(covariance);
-%  theta = atan2(V(2,1),V(1,1))
-%  
-%  plot( median( im1a.I_a(mask.tumor) ), median( im2a.I_a(mask.tumor) ), 'k^','MarkerFaceColor','black','MarkerSize',5 );
-%  ellipse(abs(median( im2a.I_a(mask.tumor) ) - prctile( im2a.I_a(mask.tumor) ,32 )), ...
-%          abs(median( im1a.I_a(mask.tumor) ) - prctile( im1a.I_a(mask.tumor) ,32 )), 0,...%theta, ...
-%          median( im1a.I_a(mask.tumor) ), median( im2a.I_a(mask.tumor) ),'r');
-%  
-%      
-%  hold on
-%  covariance = cov([im1a.I_a(mask.brain) im2a.I_a(mask.brain)]);
-%  [V, eigenval ] = eig(covariance);
-%  theta = atan2(V(2,1),V(1,1));
-%  
-%  plot( median( im1a.I_a(mask.brain) ), median( im2a.I_a(mask.brain) ), 'ko','MarkerFaceColor','black','MarkerSize',5 );
-%  ellipse(abs(median( im2a.I_a(mask.brain) ) - prctile( im2a.I_a(mask.brain) ,32 )), ...
-%          abs(median( im1a.I_a(mask.brain) ) - prctile( im1a.I_a(mask.brain) ,32 )), 0,...%theta, ...
-%          median( im1a.I_a(mask.brain) ), median( im2a.I_a(mask.brain) ),'g');
-%      
-% end
-% 
-% h2 = figure;
-% hold on;
-% for i = 1:length(fnames)
-%      load(['X:\#5 - Data\# 2015 RAT F98 ZEISS IMAGING\# F98 Rats Batch 4 (F401-F415)\processed\',...
-%      fnames{i} 'wrp.mat']);
-%      
-%  figure(h2)
-%  
-%  hold on
-%  plot(im1a.I_a(mask.tumor),im3a.I_a(mask.tumor),'g.','MarkerSize',1);
-% 
-%  
-%  hold on
-%  covariance = cov([im1a.I_a(mask.tumor) im3a.I_a(mask.tumor)]);
-%  [V, eigenval ] = eig(covariance);
-%  theta = atan2(V(2,1),V(1,1));
-%  
-%  plot( median( im1a.I_a(mask.tumor) ), median( im3a.I_a(mask.tumor) ), 'k^','MarkerFaceColor','black','MarkerSize',5 );
-%  ellipse( abs(median( im3a.I_a(mask.tumor) ) - prctile( im3a.I_a(mask.tumor) ,5 )), ...
-%           abs(median( im1a.I_a(mask.tumor) ) - prctile( im1a.I_a(mask.tumor) ,5 )), theta,...
-%           median( im1a.I_a(mask.tumor) ), median( im3a.I_a(mask.tumor) ),'k');
-%  
-% hold on
-% plot(im1a.I_a(mask.brain),im3a.I_a(mask.brain),'g.','MarkerSize',1);
-% 
-% 
-%  hold on
-%  covariance = cov([im1a.I_a(mask.brain) im3a.I_a(mask.brain)]);
-%  [V, eigenval ] = eig(covariance);
-%  theta = atan2(V(2,1),V(1,1));
-%  
-%  plot( median( im1a.I_a(mask.brain) ), median( im3a.I_a(mask.brain) ), 'ko','MarkerFaceColor','black','MarkerSize',5 );
-%  ellipse(abs(median( im3a.I_a(mask.brain) ) - prctile( im3a.I_a(mask.brain) ,5 )), ...
-%          abs(median( im1a.I_a(mask.brain) ) - prctile( im1a.I_a(mask.brain) ,5 )), theta, ...
-%          median( im1a.I_a(mask.brain) ), median( im3a.I_a(mask.brain) ),'k');
-% 
-% end
-% 
-% %   figure(h1)
-% %      hold on
-% %      errorbarxy( median( im1a.I_a(mask.tumor) ), median( im2a.I_a(mask.tumor)), ...
-% %          abs(median( im1a.I_a(mask.tumor) ) - prctile( im1a.I_a(mask.tumor) ,25 )), ...
-% %          abs(median( im2a.I_a(mask.tumor) ) - prctile( im2a.I_a(mask.tumor) ,25 )),{'go--', 'g', 'g'});
-% %      hold on;
-% %      errorbarxy( median( im1a.I_a(mask.brain) ), median( im2a.I_a(mask.brain)), ...
-% %          abs(median( im1a.I_a(mask.brain) ) - prctile( im1a.I_a(mask.brain) ,25 )), ...
-% %          abs(median( im2a.I_a(mask.brain) ) - prctile( im2a.I_a(mask.brain) ,25 )),{'ro--', 'r', 'r'});
-% %      hold on;
-% %      
-% %      figure(h2)
-% %      hold on
-% %      errorbarxy( median( im1a.I_a(mask.tumor) ), median( im3a.I_a(mask.tumor)), ...
-% %          abs(median( im1a.I_a(mask.tumor) ) - prctile( im1a.I_a(mask.tumor) ,25 )), ...
-% %          abs(median( im3a.I_a(mask.tumor) ) - prctile( im3a.I_a(mask.tumor) ,25 )),{'go--', 'g', 'g'});
-% %      hold on;
-% %      errorbarxy( median( im1a.I_a(mask.brain) ), median( im3a.I_a(mask.brain)), ...
-% %          abs(median( im1a.I_a(mask.brain) ) - prctile( im1a.I_a(mask.brain) ,25 )), ...
-% %          abs(median( im3a.I_a(mask.brain) ) - prctile( im3a.I_a(mask.brain) ,25 )),{'ro--', 'r', 'r'});
-% %      hold on;
-% %      
-%   end
-%      
-% 
-% figure(h1)
-% xlabel('ABY029 (RFU)'); ylabel('PpIX (RFU)');
-% 
-% figure(h2)
-% xlabel('ABY029 (RFU)'); ylabel('IRDye680 (RFU)');
-% 
-% 
-% xlabel('ABY029 (RFU)');
-% ylabel('PpIX (RFU)');
-% zlabel('IRD680 (RFU)');
-% % do some ROC analysis,
-% [X,Y,T,AUC] = perfcurve(double(2.*mask.brain(:) + 1.*mask.tumor(:)),mprob(:),1);
-% 
-% 
-% 
-% for i = 1:10
-%     
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
